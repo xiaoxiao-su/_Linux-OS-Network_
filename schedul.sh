a54009699a0052be31e6c967f5b54ccb1789da9b
xiaoxiao-su <602507363@qq.com>
2016-01-25 13:22:00 +0800

    Update schedul.sh

--- schedul.sh
+++ schedul.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-					     #shell脚本编写
+			     #shell脚本编写
 
-bar=''                   #定义变量不需要类型,不用分号
+bar=''                       #定义变量不需要类型,不用分号
 index=0
 
-flag=("|" "/" "-" "\\")  #定义数组，\ 和下面的 % 都属于特殊字符，所以需要转义
+flag=("|" "/" "-" "\\")      #定义数组，\ 和下面的 % 都属于特殊字符，所以需要转义
 
-while [ $index -le 100 ] #while语句进行条件判断
+while [ $index -le 100 ]     #while语句进行条件判断
 do
 	pos=index%4          #或者 let pos=index%4
 	bar=${bar}#          # ${bar}是取bar的值
